6849d2cd898a7537439732ae0d6af080b688a3f9
ardaj <kolostest@email.cz>
2026-03-30 20:10:51 +0200

    úprava hratelnosti

--- Backend/Models/GameRoom.cs
+++ Backend/Models/GameRoom.cs
@@ -1,31 +1,4 @@
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-
-namespace RoguelikeCardGame.Models
-{
-    // Pomocná třída pro uložení dat o zahrané kartě v daném tahu
-    public class CardPlayData
-    {
-        public string CardId { get; set; }
-        public int KarmaShift { get; set; }
-        public int Damage { get; set; }
-    }
-
-    public class GameRoom
-    {
-        public string RoomName { get; set; }
-        public List<Player> Players { get; set; } = new List<Player>();
-        public int CurrentKarma { get; set; } = 0;
-        
-        // Životy společného nepřítele (parametr k nastavení)
-        public int EnemyHp { get; set; } = 100; 
-
-        // Paměť pro aktuální tah: Kdo (jméno) co zahrál (data o kartě)
-        public ConcurrentDictionary<string, CardPlayData> PlayedCardsThisTurn { get; set; } = new ConcurrentDictionary<string, CardPlayData>();
-
-        public GameRoom(string roomName)
-        {
-            RoomName = roomName;using System;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
@@ -69,7 +42,7 @@ namespace RoguelikeCardGame.Models
         public int CurrentKarma { get; set; } = 0;
         public int EnemyHp { get; set; } = 100; 
         
-        // --- NOVÉ: EKONOMIKA A POSTUP ---
+        // --- EKONOMIKA A POSTUP ---
         public int TeamGold { get; set; } = 50; // Společná pokladna
         public List<string> TeamRelics { get; set; } = new List<string>(); // Získané relikvie
         
@@ -111,9 +84,6 @@ namespace RoguelikeCardGame.Models
         public MapNode GetCurrentNode()
         {
             return Map.FirstOrDefault(n => n.Id == CurrentNodeId);
-        }
-    }
-}
         }
     }
 }
